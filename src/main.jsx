f37d757b1fed0a10b1b48327be7de0cbe5d099cc
rishav <singhrishav08@gmail.com>
2025-10-09 06:13:42 +0530

    deployment fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { QuoteProvider } from './state/QuoteContext'
 
 createRoot(document.getElementById('root')).render(
 <React.StrictMode>
-<BrowserRouter>
+<BrowserRouter basename="/quote-gen/">
 <QuoteProvider>
 <App />
 </QuoteProvider>
